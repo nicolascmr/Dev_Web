a0977688f224d152a68d40a948e200bb161ca3b3
Camera Nicolas <o22404919@or-iut-i003-f09.iut45.univ-orleans.fr>
2025-09-09 16:59:43 +0200

    Debut du TP2, arreté au chargement du YAML

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
 #>>>import random, string, os
 #>>>"".join([random.choice(string.printable) for _ in os.urandom(24) ] )
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'monApp.db')
 SECRET_KEY = "2lzUl{$*D6#`8uXqlU."
+ABOUT = "Bienvenue sur la page à propos de Flask !"
+CONTACT = "Coordonnées de contact : "
--- monApp/app.py
+++ monApp/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
-app = Flask ( __name__ )
+app = Flask(__name__)
 
 # Config options - Make sure you created a 'config.py' file.
 app.config.from_object('config')
 # To get one variable, tape app.config['MY_VARIABLE']
+# Create database connection object
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy(app)
+#db.init_app(app)
